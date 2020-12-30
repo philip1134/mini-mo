2fab1d9265b4aa256e208a42447964371d398efe
philip1134 <philip1134@imior.com>
2020-12-30 22:42:00 +0800

    fix(generator): convert case name to full module name

--- minimo/ext/generator.py
+++ minimo/ext/generator.py
@@ -203,7 +203,7 @@ def create_new_cases(cases, author=None):
                                          "%s.root" % ctx.app.name)))
 
                 # copy files
-                config["case_name"] = re.sub(r"\w", ".", case)
+                config["case_name"] = re.sub(r"\W+", ".", case)
                 copy_template_folder(target, template_dir, ".mako", config)
                 success_cases.append(case)
 
