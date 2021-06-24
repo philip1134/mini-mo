9d55e6418955a98bdfbed363f5c061df55e2fb56
philip1134 <philip1134@imior.com>
2021-06-24 13:44:08 +0800

    fix(ext): resume extension in instance folder

---
+++ minimo/templates/projects/task/ext/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding:utf-8 -*-
+
+# import your extensions here, and insert them into __autoload__, minimo
+# will auto load the extended cli.
+
+# from .my-extension import my-cli
+
+__autoload__ = (
+    # my-cli
+)
+
+# end
